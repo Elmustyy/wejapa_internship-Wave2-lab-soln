a530484f4f25223f36e124a13411cf5c9a3440f7
Elmustyy <49339501+Elmustyy@users.noreply.github.com>
2020-08-01 04:00:23 -0700

    Add files via upload

---
+++ lab1 of 3.py
@@ -0,0 +1,6 @@
+list_items =[1,2,4,7]
+print('len of list:\n',len(list_items))
+print('appending 5 to list:\n',list_items.append(5))
+print('adding 9 to index 0:\n',list_items.insert(0,9))
+print('deleting 4 from the list:\n',list_items.del[2])
+print('slicing the list to print out the last number:\n',list_items[-1])
